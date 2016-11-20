a0ffaae17b91a76e0d2bd9df1580214aeb8a97c9
Cejkis <cejkis@gmail.com>
2016-11-20 18:34:07 +0100

    added comments

--- Main.m
+++ Main.m
@@ -2,6 +2,7 @@
 clear;
 ROUNDS = 100;
 
+% creating handlers for all strategies
 alwaysCoop = @AlwaysCooperate;
 alwaysDefect = @AlwaysDefect;
 titForTat = @TitForTat;
@@ -11,6 +12,7 @@ random = @Random;
 strategiesHandles = {alwaysCoop, alwaysDefect, titForTat, turnEvil, random};
 nrOfStrategies = length(strategiesHandles);
 
+% running them against each other
 results = zeros(nrOfStrategies);
 
 for i = 1:nrOfStrategies
@@ -20,17 +22,14 @@ for i = 1:nrOfStrategies
        score = [0,0];
        
        for r = 1: ROUNDS
-            p1 = strategiesHandles{i}(history);
+            p1 = strategiesHandles{i}(history); % get the move of each prisoner
             p2 = strategiesHandles{j}(history);
-            history = [history; p1 p2];
-            utilities = PrisonersRound(p1, p2);
-            score(1) = score(1) + utilities(1);
-            score(2) = score(2) + utilities(2);
+            history = [history; p1 p2]; % update history matrix
+            utilities = PrisonersRound(p1, p2); % compute utilities for both prisoners
+            score = score + utilities; 
        end
        
-       results(i,j) = score(1);
-       
-       %fprintf('',)
+       results(i,j) = score(1); % I dont know how to print nicely both
        
     end
 end
