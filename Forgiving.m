3d4c251f9244f5c87b92efa521e29c2c82457a5a
Anton Danielsson <daanton@student.chalmers.se>
2016-12-15 11:14:53 +0100

    Added forgiving strategy

---
+++ Forgiving.m
@@ -0,0 +1,40 @@
+classdef Forgiving < Strategy & handle
+    %Forgiving. Forgives one mistake of the opponent if the strategy also
+    %has made a previous mistake.
+    
+    properties
+        id = 0;
+        mistOk = 0;
+    end
+    
+    methods
+        function [ out ] = Action( obj,history,~ )
+            if size(history,1) == 0
+                out = 1;
+                return;
+            else 
+                if (history(end, 1) == 0)
+                    obj.mistOk = 1;
+                end
+                
+                if ((history(end, 2) == 0) && obj.mistOk)
+                    obj.mistOk = 0;
+                    out = 1;
+                elseif (history(end, 2))
+                    out = 1;
+                else
+                    out = 0;
+                end        
+            end
+        end
+        
+        function out = get_id(obj)
+            out = obj.id;
+        end
+        
+        function set_id(obj, id)
+            obj.id = id;
+        end
+    end
+    
+end
